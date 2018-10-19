42f2371700e82e0e35fa1b443171fa9c17dadc7a
Piyush Sharma <piyushsharma@piyushs-mac-mini.local>
2018-10-19 23:40:57 +0530

    One to one relationship.

---
+++ RelationsWithFaults/RelationsWithFaults/HomeVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomeVC.swift
+//  RelationsWithFaults
+//
+//  Created by Piyush Sharma on 19/10/18.
+//  Copyright © 2018 Piyush Sharma. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
